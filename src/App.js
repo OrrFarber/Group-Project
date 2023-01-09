6af7c811cbb1440c25e067a7bb7804854bb8b303
OrrFarber <orr.farber@tichonet.com>
2023-01-09 17:07:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "9796feb253msh65f876a3c1039f8p1facffjsnb9a3c57b73f1",
+        "X-RapidAPI-Host": "exercises-by-api-ninjas.p.rapidapi.com",
+      },
+    };
+
+    fetch(
+      "https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?muscle=biceps",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hi</h1>
     </div>
   );
 }
 
 export default App;
+// https://itnext.io/react-push-notifications-with-hooks-d293d36f4836
