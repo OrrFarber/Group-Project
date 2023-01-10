136e1dc698027ab50eb418928cddc03773e070f2
OrrFarber <Orr.farber@tichonet.com>
2023-01-10 10:14:39 +0200

    added routes

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+export default function HomePage() {
+  return (
+    <div className="HomePage">
+      <ul>
+        <img
+          src="C:\Users\orrfa\Desktop\code\group-project\workout-plans\public\fitness-body-building-logo.webp"
+          alt="fitness logo"
+        ></img>
+        
+      </ul>
+
+      <button>Login</button>
+      <button>Sign up</button>
+    </div>
+  );
+}
